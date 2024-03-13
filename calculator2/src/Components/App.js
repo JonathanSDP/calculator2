026e9ba3e59f4aa834784699042c9f444747ccf3
JonathanSDP <jonas1993seba@gmail.com>
2024-03-12 23:13:40 -0300

    subida de calculadora

---
+++ calculator2/src/Components/App.js
@@ -0,0 +1,16 @@
+import Display from "./Display"
+import ButtonPanel from "./ButtonPanel"
+import  {Component} from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hola</h1>
+      </div>
+    )
+  }
+}
+
+
+export default App;
